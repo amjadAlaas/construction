a445905ec4fff77fbcd2c9c8e76021538f09ba9a
unknown <amjad.alass94@gmail.com>
2022-08-08 06:51:33 +0200

    complete edit

--- js/script.js
+++ js/script.js
@@ -1,37 +1,68 @@
 let statsCounters = document.querySelectorAll(".statistics .content span");
 let speed = 200;
 
+let statsSection = document.querySelector(".statistics");
 
-function xx() {
-    statsCounters.forEach((counter, index) => {
-    
-        function updateCounter() {
-            const targetNumber = +counter.dataset.target;
-            const initialNumber = +counter.innerText;
-            const incPerCount = targetNumber / speed;
-            if (initialNumber < targetNumber) {
-                counter.innerText = Math.ceil(initialNumber + incPerCount);
-                setTimeout(updateCounter, 40);
+window.onscroll = function () {
+
+    //Skills Offset Top
+    let statsOffsetTop = statsSection.offsetTop;
+
+    // Skills outerHeight
+    let statsOuterHeight = statsSection.offsetHeight;
+
+    //Window Height
+    let windowHeight = this.innerHeight;
+
+    //Window ScrollTop
+    let windowScrollTop = this.scrollY;
+
+    if (windowScrollTop >= (statsOffsetTop + statsOuterHeight - windowHeight)) {
+
+        let speed = 200;
+        statsCounters.forEach((counter) => {
+
+            function updateCounter() {
+                const targetNumber = +counter.dataset.target;
+                const initialNumber = +counter.innerText;
+                const incPerCount = targetNumber / speed;
+                if (initialNumber < targetNumber) {
+
+                    counter.innerText = Math.ceil(initialNumber + incPerCount);
+                    setTimeout(updateCounter, 50);
+
+                }
+                
             }
-        }
-        updateCounter();
-        
-    });
+            updateCounter();
+
+        });
+
+    }
+
 }
 
+// Carousel
+let boxes = document.querySelectorAll('.carousel .box');
+let bullets = document.querySelectorAll('.carousel .bullets span');
+let i = 0;
 
+var slideShow = function () {
 
-window.addEventListener("scroll", function () {
-    
-    
-    //window scroll top
-    let statsSection = document.querySelector(".statistics");
-    const windowScrollTop = this.scrollY;
-    if (windowScrollTop > statsSection.outerHeight + statsSection.offsetHeight) {
-        xx();
-        ;
+    if (i < boxes.length) {
+        boxes.forEach((box) => {
+            box.classList.remove("active");
+        });
+        bullets.forEach((box) => {
+            box.classList.remove("active");
+        });
+        boxes[i].classList.add("active");
+        bullets[i].classList.add("active");
+        i++;
+    } else {
+        i = 0;
     }
-});
-console.log("y");
-        console.log(statsSection);
-        console.log(statsSection.offsetHeight);
+    
+    setTimeout(slideShow, 1000);
+};
+slideShow();
